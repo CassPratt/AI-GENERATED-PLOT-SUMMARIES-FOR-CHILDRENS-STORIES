668b9f6ddb002d05e1cff3e2cb38db6475bfaf27
Cassandra Pratt Romero <caprattr@microsoft.com>
2023-11-13 18:54:10 -0600

    Update Key vault parameters

--- kv_secrets.py
+++ kv_secrets.py
@@ -2,10 +2,7 @@ import os
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
-os.environ['KEY_VAULT_NAME'] = 'AI-GPSCS-KeyVault'
-
-keyVaultName = os.environ['KEY_VAULT_NAME']
-KVUri = f"https://{keyVaultName}.vault.azure.net"
+KVUri = f"https://AI-GPSCS-KeyVault.vault.azure.net"
 
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=KVUri, credential=credential)
